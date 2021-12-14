7874dc47098b790823d8ea469ca212b236fa4c52
Vlad <guslitge@student.gu.se>
2021-12-14 01:09:43 +0100

    Clear mqtt history

--- index.js
+++ index.js
@@ -9,24 +9,30 @@ client.on("connect", e => {
     console.log("connected")
     client.subscribe("/dentistimo/#", e => {
         client.on("message", (topic, m) => {
-            try {
-                let message = JSON.parse(m)
-                if (message.request === 'post') {
-                    postRequest(message.url, JSON.parse(message.data)).then(data => {
-                        let response = { "id": message.id, "data": data }
-                        client.publish(topic, JSON.stringify(response))
-                    })
-                } else if (message.request === 'get') {
-                    getRequest(message.url).then(data => {
-                        let response = { "id": message.id, "data": data }
-                        client.publish(topic, JSON.stringify(response))
-                    })
+            if (m.length !== 0){
+                try {
+                    let message = JSON.parse(m)
+                    if (message.request === 'post') {
+                        postRequest(message.url, JSON.parse(message.data)).then(data => {
+                            let response = { "id": message.id, "data": data }
+                            client.publish(topic, JSON.stringify(response))
+                        })
+                    } else if (message.request === 'get') {
+                        getRequest(message.url).then(data => {
+                            let response = { "id": message.id, "data": data }
+                            client.publish(topic, JSON.stringify(response))
+                        })
+                    } else if (message.confirm === "received"){
+                        console.log("a")
+                        client.unsubscribe(topic)
+                        client.publish(topic, "")
+                    }
+                    console.log(message)
+                } catch (e) {
+                    let response = { "id": topic.split('/').pop(), "data": "400 Bad Request" }
+                    client.publish(topic, JSON.stringify(response))
                 }
-                console.log(message)
-            } catch (e) {
-                let response = { "id": topic.split('/').pop(), "data": "400 Bad Request" }
-                client.publish(topic, JSON.stringify(response))
-            }
+            } 
         })
     })
 })
