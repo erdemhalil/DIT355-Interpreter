faa4fa2682e9e8bd20680e1c00ccf9ce264a43df
Vlad <guslitge@student.gu.se>
2021-12-16 16:20:11 +0100

    bugfix

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ const Api = axios.create({
     baseURL: 'http://localhost:8000/api'
 })
 
+var  options = { qos: 1 }
+
 client.on("connect", e => {
     console.log("connected")
     client.subscribe("/dentistimo/#", e => {
-        client.on("message", (topic, m) => {
+        client.on("message", (topic, m, option) => {
             if (m.length !== 0){
                 try {
                     let message = JSON.parse(m)
                     if (message.request === 'post') {
                         postRequest(message.url, JSON.parse(message.data)).then(data => {
-                            let response = { "id": message.id, "data": data }
-                            client.publish(topic, JSON.stringify(response))
+                            let response = { "id": message.id, "response": "response", "data": data }
+                            return client.publish(topic, JSON.stringify(response), options)
                         })
                     } else if (message.request === 'get') {
                         getRequest(message.url).then(data => {
-                            let response = { "id": message.id, "data": data }
-                            client.publish(topic, JSON.stringify(response))
+                            let response = { "id": message.id, "response": "response", "data": data }
+                            return client.publish(topic, JSON.stringify(response), options)
                         })
                     }
+                    console.log(option)
                 } catch (e) {
-                    let response = { "id": topic.split('/').pop(), "data": "400 Bad Request" }
-                    client.publish(topic, JSON.stringify(response))
+                    let response = { "id": topic.split('/').pop(), "response": "response", "data": "400 Bad Request" }
+                    return client.publish(topic, JSON.stringify(response), options)
                 }
             } 
         })
